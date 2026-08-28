0c59df4c0ec7a57bd7f86e3a0e180a6a898432da
maks_p <mpekur.dev@gmail.com>
2026-08-29 00:19:17 +0200

    refactor(admin): move the user menu from the sidebar into the header

--- apps/admin/src/routes/(protected)/_layout.tsx
+++ apps/admin/src/routes/(protected)/_layout.tsx
@@ -65,7 +65,7 @@ function ProtectedLayout() {
     <SidebarProvider style={sidebarStyle}>
       <AppSidebar operator={operator} />
       <SidebarInset>
-        <SiteHeader />
+        <SiteHeader operator={operator} />
         <div className="@container/main flex flex-1 flex-col gap-4 py-4 md:gap-6 md:py-6">
           <Outlet />
         </div>
